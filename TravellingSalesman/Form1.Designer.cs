a31b8f9e270a9acb8a90c417666db4146941576c
Ярослав Соломенцев <yaroslav.solo@yandex.ru>
2019-05-19 13:12:00 +0300

    Добавьте файлы проекта.

---
+++ TravellingSalesman/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace TravellingSalesman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.QuantityOfPoints = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QuantityOfPoints)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(458, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(675, 651);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(72, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Run";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 201);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(335, 323);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Number of cities:";
+            // 
+            // QuantityOfPoints
+            // 
+            this.QuantityOfPoints.Location = new System.Drawing.Point(194, 49);
+            this.QuantityOfPoints.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.QuantityOfPoints.Name = "QuantityOfPoints";
+            this.QuantityOfPoints.Size = new System.Drawing.Size(120, 22);
+            this.QuantityOfPoints.TabIndex = 6;
+            this.QuantityOfPoints.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(194, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "1e+20";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Initial temperature:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 691);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.QuantityOfPoints);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(1163, 738);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QuantityOfPoints)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown QuantityOfPoints;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TravellingSalesman/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravellingSalesman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            g.FillRectangle(new SolidBrush(Color.Aquamarine), 0, 0, 512, 512);
+            pictureBox.Image = image;
+        }
+
+        public static Random gen = new Random();
+        static Bitmap image = new Bitmap(512, 512);
+        static Graphics g = Graphics.FromImage(image);
+        static Point[] arr;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.FillRectangle(new SolidBrush(Color.Aquamarine), 0, 0, 512, 512);
+            arr = new Point[(int)QuantityOfPoints.Value];          
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Point((float)(gen.NextDouble() * 500 + 6), (float)(gen.NextDouble() * 500 + 6));
+                g.FillEllipse(new SolidBrush(Color.Red), arr[i].X - 2, arr[i].Y - 2, 5, 5);
+            }
+
+            pictureBox.Image = image;
+            pictureBox.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g.FillRectangle(new SolidBrush(Color.Aquamarine), 0, 0, 512, 512);           
+
+            if (arr != null)
+            {
+                int[] path = new int[arr.Length];
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    g.FillEllipse(new SolidBrush(Color.Red), arr[i].X - 2, arr[i].Y - 2, 5, 5);
+                }
+
+                double.TryParse(textBox2.Text, out double tmax);
+                double tmin = 1;
+
+                DateTime time = DateTime.Now;
+                path = Annealing(arr, ref tmax, ref tmin);
+
+                textBox1.AppendText($"Time: {DateTime.Now - time}\r\n");
+                textBox1.AppendText($"Iterations: {tmax}, distance: {tmin:F3}\r\n");
+
+                DrawPath(path);
+                pictureBox.Image = image;
+                pictureBox.Invalidate();
+            }        
+        }
+
+        public static void DrawPath(int[] candidate)
+        {
+            Pen p = new Pen(new SolidBrush(Color.Black));
+
+            for (int i = 0; i < candidate.Length - 1; i++)
+            {
+                g.DrawLine(p, arr[candidate[i]].X, arr[candidate[i]].Y, arr[candidate[i + 1]].X, arr[candidate[i + 1]].Y);
+            }
+
+            g.DrawLine(p, arr[candidate[0]].X, arr[candidate[0]].Y, arr[candidate[arr.Length - 1]].X, arr[candidate[arr.Length - 1]].Y);
+        }
+
+        public static int[] Annealing(Point[] arr, ref double tmax, ref double tmin)
+        {
+            int[] candidate = new int[arr.Length];
+            int[] newcandidate = new int[arr.Length];
+            int num = 1;
+
+            for (int i = 0; i < arr.Length ; i++)
+            {
+                candidate[i] = i;
+            }
+
+            double candval = F(arr, candidate);
+
+            double bestval = candval;
+            int[] bestarr = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bestarr[i] = candidate[i];
+            }
+
+            while (tmax > tmin)
+            {              
+                newcandidate = NewCandidate1(candidate);
+                double newcandval = F(arr, newcandidate);
+
+                if (candval >= newcandval)
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        candidate[i] = newcandidate[i];
+                    }
+
+                    candval = newcandval;
+                    if(newcandval < bestval)
+                    {
+                        bestval = newcandval;
+
+                        for (int i = 0; i < arr.Length; i++)
+                        {
+                            bestarr[i] = newcandidate[i];
+                        }
+                    } 
+                }                  
+                else if (GaussianNorm(candval - newcandval, tmax))
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        candidate[i] = newcandidate[i];
+                    }
+
+                    candval = newcandval;
+                }
+
+                tmax *= 1 - 0.0003;
+
+                num++;
+            }
+
+            tmax = num;
+            tmin = bestval;
+
+            return bestarr;
+        }
+
+        private static bool GaussianNorm(double factor, double temperature)
+        {
+            if (gen.NextDouble() < Math.Exp(factor / temperature))
+                return true;
+            else
+                return false;
+        }
+
+        private static double F(Point[] arr, int[] candidate)
+        {
+            double distance = 0;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                distance += Distance(arr[candidate[i]], arr[candidate[i + 1]]);
+            }
+
+            return distance + Distance(arr[candidate[0]], arr[candidate[arr.Length - 1]]);
+        }
+
+        public static int[] NewCandidate1(int[] candidate)
+        {
+            int[] reversed = new int[candidate.Length];
+
+            for (int i = 0; i < candidate.Length; i++)
+            {
+                reversed[i] = candidate[i];
+            }
+
+            int temp = gen.Next(0, arr.Length - 2);
+
+            Array.Reverse(reversed, temp, gen.Next(2, candidate.Length - temp));
+
+            return reversed;
+        }
+
+        public static int[] NewCandidate2(int[] candidate)
+        {
+            int[] reversed = new int[candidate.Length];
+
+            for (int i = 0; i < candidate.Length; i++)
+            {
+                reversed[i] = candidate[i];
+            }
+
+            for(int i = 0; i < 2; i++)
+            {
+                int a = gen.Next(0, arr.Length - 1);
+                int b = gen.Next(0, arr.Length - 1);
+
+                int t = reversed[a];
+                reversed[a] = reversed[b];
+                reversed[b] = t;
+            }
+
+            return reversed;
+        }
+
+        public static double Distance(Point p1, Point p2)
+        {
+            return Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));
+        }
+    }
+
+    public class Point
+    {
+        public float X { get; set; } 
+
+        public float Y { get; set; }
+
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }     
+    }
+}
